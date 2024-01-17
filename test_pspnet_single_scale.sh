10543d97f47cc572e991ba174cf6a15e24c689fd
Hyggge <hyggge6@gmail.com>
2024-01-17 23:36:25 +0800

    feat: 增加 single scale test

---
+++ test_pspnet_single_scale.sh
@@ -0,0 +1,8 @@
+python3 -u test_pspnet_single_scale.py ApolloScape PSPNet train bdd100k_val \
+                          --lr 0.01 \
+                          --gpus 0 \
+                          --npb \
+                          --resume trained_model/pspnet_46_6.pth.tar \
+                          --test_size 1410 \
+                          -j 10 \
+                          -b 1
